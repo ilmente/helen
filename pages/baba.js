c91b7ce733d0152a46617f05ab2298f224196322
ilmente <alessandro.bellini@spryker.com>
2019-03-15 13:03:10 +0200

    add photos and zip

--- pages/baba.js
+++ pages/baba.js
@@ -11,15 +11,36 @@ export default withRouter(({ router }) => {
     return isAuthorized ? (
         <Layout title="Baba">
             <h1>Hello baba!</h1>
-            <Figure image="test" caption='test' />
+            <p>
+                So! Here we are!
+            </p>
+            <Figure image="face" caption='wave' />
             <p>
                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
             </p>
             <Gallery images={[
-                'test', 'test', 'test', 'test'
-            ]} caption="test"/>
+                'odessa1', 'odessa2', 'odessa3'
+            ]} caption="Odessa"/>
             <p>
                 In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+            </p>
+
+            <hr/>
+
+            <h4>Disclaimer</h4>
+            <p>
+                This site has been totally <em>hand crafted</em>
+                in around 12 hours stolen from work, sleep and sex. 👉👌🙈
+                It's based only on nextjs and react.
+                I'm writing these details down just to brag. 😎
+            </p>
+
+            <hr />
+
+            <h4>Photos</h4>
+            <p>
+                If you want to take a better look at the images,
+                you can <a href="/static/files/photos.zip">download them from here (~63mb)</a>.
             </p>
         </Layout>
     ) : Nope()
